fd02cb4859e8f774ef16ceb3152aaac51350698d
Ifriism <idafm2@hotmail.com>
2025-06-13 16:55:14 +0200

    Minor adjustments

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -22,18 +22,15 @@ export default function Dictionary(props) {
 
     function search() {
  // documentation: https://www.shecodes.io/learn/apis/dictionary
-        let apiKey = "fea6579f5ctf53bb7491ae80ac32o60f";
-        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
+        const apiKey = "fea6579f5ctf53bb7491ae80ac32o60f";
+        const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
         axios.get(apiUrl).then(handleResponse);
 
-        const pexelsApiKey = "jnCi4xOV94kKqoKqdEca2F0UdtCzGKSBfi6QrK1WhihqMjx1GYfbTSwV";
+        const pexelsApiKey = process.env.REACT_APP_API_KEY;
         const headers = { Authorization: ` ${pexelsApiKey}` };
         const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
         axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     } 
-
-
-
 
     function handleSubmit(event) {
         event.preventDefault();

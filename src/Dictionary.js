baa166448c780840ae4f88d855360bb52e91e4df
Ifriism <idafm2@hotmail.com>
2025-06-13 16:09:27 +0200

    Added photo API and photo component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 
 export default function Dictionary(props) {
     const [keyword, setKeyword] = useState(props.defaultKeyword);
     const [results, setResults] = useState(null);
     const [loaded, setLoaded] = useState(false);
+    const [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data);
         setResults(response.data);
+    }
+
+    function handlePexelsResponse(response) {
+        console.log(response);
+        setPhotos(response.data.photos);
     }
 
     function search() {
@@ -19,7 +25,15 @@ export default function Dictionary(props) {
         let apiKey = "fea6579f5ctf53bb7491ae80ac32o60f";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
         axios.get(apiUrl).then(handleResponse);
+
+        const pexelsApiKey = "jnCi4xOV94kKqoKqdEca2F0UdtCzGKSBfi6QrK1WhihqMjx1GYfbTSwV";
+        const headers = { Authorization: ` ${pexelsApiKey}` };
+        const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     } 
+
+
+
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -47,6 +61,7 @@ export default function Dictionary(props) {
                     </div>
                 </section>
              <Results results={results} />
+             <Photos photos={photos} />
             </div>
         );
 
